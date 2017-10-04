b70342b0b0b839533ae0fd4372d855f9745c727e
Simon Owen <simon.owen@thehutgroup.com>
2017-10-04 07:42:53 +0100

    Site updated: 2017-10-04 07:42:37

--- sw-config.js
+++ sw-config.js
@@ -1,10 +1,1 @@
-module.exports = {
-  staticFileGlobs: [
-    'public/css/**.css',
-    'public/**/**.html',
-    'public/**/**.jpg',
-    'public/**/**.png',
-    'public/js/**.js'
-  ],
-  stripPrefix: 'public',
-}
+module.exports={staticFileGlobs:["public/css/**.css","public/**/**.html","public/**/**.jpg","public/**/**.png","public/js/**.js"],stripPrefix:"public"};
